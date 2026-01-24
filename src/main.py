cdb7da59b41b86b72caff26a1db6590d8ed4d9ef
fat-ai <67062092+fat-ai@users.noreply.github.com>
2026-01-24 21:31:11 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ import requests
 import csv
 import queue
 from typing import Dict, List, Set
+from webhook_server import start_server, trade_queue
 from fastapi import FastAPI, Request
 
 # --- MODULE IMPORTS ---
