8ba54701c5849da8108a836b5a13ee191ac296e9
fat-ai <67062092+fat-ai@users.noreply.github.com>
2026-02-28 16:28:08 +0000

    Update data.py

--- src/data.py
+++ src/data.py
@@ -106,7 +106,7 @@ class MarketMetadata:
                     for t in tokens:
                         if t not in self.token_to_fpmm:
                             self.token_to_fpmm[t] = mid
-                
+            except: continue    
 
     async def _fetch_clob_strict(self, session):
         """
