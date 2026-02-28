ebb68bbbb231db725511971ffb4e1b1f896d6b18
fat-ai <67062092+fat-ai@users.noreply.github.com>
2026-02-28 21:33:44 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -75,7 +75,6 @@ class LiveTrader:
         all_tokens = []
         for mkt in self.metadata.markets.values():
             for tok in mkt['tokens'].values():
-               log.info(tok)
                all_tokens.append(tok)
         
         print(f"✅ Metadata Loaded. Subscribing to ALL {len(all_tokens)} assets...")
